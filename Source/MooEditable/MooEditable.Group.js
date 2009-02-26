63f56104458ebebe018311bd995915b02ad9cafa
Ryan Mitchell <ryan@rtnetworks.net>
2009-02-27 00:25:01 +0800

    Changes MooEditable.Group to work differently - this approach is much neater, and should be affected less by core changes
    Signed-off-by: Lim Chee Aun <cheeaun@gmail.com>

--- Source/MooEditable/MooEditable.Group.js
+++ Source/MooEditable/MooEditable.Group.js
@@ -8,55 +8,59 @@ License:
 
 MooEditable.Group = new Class({
 
-	Extends: MooEditable,
-		
-	initialize: function(toolbarEl, editorEl, options){
+	Implements: [Options],
+    
+    options : {
+        'actions' : 'bold italic underline strikethrough | insertunorderedlist insertorderedlist indent outdent | undo redo | createlink unlink | urlimage | toggleview'
+    },
+    
+    elements : [],
+    
+    initialize : function(toolbarEl,options){
 		this.setOptions(options);
 		this.actions = this.options.actions.clean().split(' ');
-		this.keys = {};
-		this.actions.each(function(action){
-			var act = MooEditable.Actions[action];
-			if (!act || !act.options) return;
-			var key = act.options.shortcut;
-			if (key) this.keys[key] = action;
-		}.bind(this));
-		this.options.toolbar = false; // no toolbars for the editors
-		this.addInstance(editorEl);
-        self = this;
+        // setup toolbar
+        var self = this;
 		this.toolbar = new MooEditable.UI.Toolbar({
 			'class': 'mooeditable-toolbar',
 			onItemAction: function(){
 				var args = $splat(arguments);
 				var item = args[0];
-				self.focus();
-				self.action(item.name, Array.slice(args, 1));
-				if (self.mode == 'iframe') self.checkStates();
+				if(self.activeEditor){
+                    self.activeEditor.focus();
+                    self.activeEditor.action(item.name, Array.slice(args, 1));
+                    if (self.activeEditor.mode == 'iframe') self.activeEditor.checkStates();
+                }
 			}
 		});
 		$(toolbarEl).adopt(this.toolbar.render(this.actions));
-	},
-	
-	addInstance: function(el){
-		this.textarea = $(el);
-		this.textarea.store('MooEditable', this);
-		this.render();
+    },
+    
+    add : function(el,options){
+		var editor = new MooEditable.Group.Item(el, this, $merge({toolbar:false},options));
+		this.elements.push(el);
+        this.activeEditor = editor;
+		return editor;
+	}
+    
+});
+
+
+MooEditable.Group.Item = new Class({
+
+    Extends : MooEditable,
+ 
+	initialize : function(el,group,options){
+		this.group = group;
+		this.parent(el,options);
 		// add focus events
-		this.textarea.addEvent('focus',function(event){ 
-			this.textarea = $(event.target); 
-			this.iframe = $(this.textarea.getProperty("id") + "-container").getChildren("iframe")[0];
-			this.win = this.iframe.contentWindow;
-			this.doc = this.win.document;
-			this.mode = 'textarea';
+		this.textarea.addEvent('focus',function(event){
+            this.group.activeEditor = this;
 		}.bindWithEvent(this));
 		// safari doesnt fire onfocus event, so make it onclick
 		this.doc.addEvent((Browser.Engine.webkit) ? 'click' : 'focus',function(event){
-			this.self.win = this.win;
-			this.self.iframe = $(this.win.name);
-			this.self.doc = this.win.document;
-			this.self.selection = new MooEditable.Selection(this.self);
-			this.self.textarea = this.self.iframe.getParent().getChildren("textarea")[0];
-			this.self.mode = 'iframe';
-		}.bindWithEvent({self:this, win:this.win}));
+            this.group.activeEditor = this;
+		}.bindWithEvent(this));
 	}
 
 });
