552df9aa54d53075c36581a653dee5e7434dd8b1
Lim Chee Aun <cheeaun@gmail.com>
2009-03-19 19:27:50 +0800

    Whitespace and code fixes.

--- Source/MooEditable/MooEditable.Group.js
+++ Source/MooEditable/MooEditable.Group.js
@@ -8,56 +8,54 @@ License:
 
 MooEditable.Group = new Class({
 
-    Implements: [Options],
+	Implements: [Options],
+	
+	options: {
+		actions: 'bold italic underline strikethrough | insertunorderedlist insertorderedlist indent outdent | undo redo | createlink unlink | urlimage | toggleview'
+	},
     
-    options : {
-        'actions' : 'bold italic underline strikethrough | insertunorderedlist insertorderedlist indent outdent | undo redo | createlink unlink | urlimage | toggleview'
-    },
-        
-    initialize : function(toolbarEl,options){
-        this.setOptions(options);
-        this.actions = this.options.actions.clean().split(' ');
-        // setup toolbar
-        var self = this;
-        this.toolbar = new MooEditable.UI.Toolbar({
-            'class': 'mooeditable-toolbar',
-            onItemAction: function(){
-                var args = $splat(arguments);
-                var item = args[0];
-                if(self.activeEditor){
-                    self.activeEditor.focus();
-                    self.activeEditor.action(item.name, Array.slice(args, 1));
-                    if (self.activeEditor.mode == 'iframe') self.activeEditor.checkStates();
-                }
-            }
-        });
-        $(toolbarEl).adopt(this.toolbar.render(this.actions));
-    },
+	initialize: function(toolbarEl, options){
+		this.setOptions(options);
+		this.actions = this.options.actions.clean().split(' ');
+		// setup toolbar
+		var self = this;
+		this.toolbar = new MooEditable.UI.Toolbar({
+			onItemAction: function(){
+				var args = $splat(arguments);
+				var item = args[0];
+                if (!self.activeEditor) return;
+                self.activeEditor.focus();
+                self.activeEditor.action(item.name, args);
+                if (self.activeEditor.mode == 'iframe') self.activeEditor.checkStates();
+			}
+		}).render(this.actions);
+		$(toolbarEl).adopt(this.toolbar);
+	},
 
-    add : function(el,options){
-        var editor = new MooEditable.Group.Item(el, this, $merge({toolbar:false},options));
-        this.activeEditor = editor;
-        return editor;
-    }
-    
+	add: function(el, options){
+		var editor = new MooEditable.Group.Item(el, this, $merge({toolbar:false}, options));
+		this.activeEditor = editor;
+		return editor;
+	}
+	
 });
 
 
 MooEditable.Group.Item = new Class({
 
-    Extends : MooEditable,
- 
-    initialize : function(el,group,options){
-        this.group = group;
-        this.parent(el,options);
-        // add focus events
-        this.textarea.addEvent('focus',function(event){
-            this.group.activeEditor = this;
-        }.bindWithEvent(this));
-        // safari doesnt fire onfocus event, so make it onclick
-        this.doc.addEvent((Browser.Engine.webkit) ? 'click' : 'focus',function(event){
-            this.group.activeEditor = this;
-        }.bindWithEvent(this));
-    }
+	Extends: MooEditable,
+
+	initialize: function(el, group, options){
+		this.group = group;
+		this.parent(el, options);
+		// add focus events
+		this.textarea.addEvent('focus', function(){
+			this.group.activeEditor = this;
+		}.bind(this));
+		// safari doesnt fire onfocus event, so make it onclick
+		this.doc.addEvent((Browser.Engine.webkit) ? 'click' : 'focus', function(){
+			this.group.activeEditor = this;
+		}.bind(this));
+	}
 
 });
