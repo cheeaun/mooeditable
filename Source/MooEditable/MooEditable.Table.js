01ea6bd6c67cf76546c185b3fb7bfe63c6486cc1
Radovan Lozej <radovan.lozej@gmail.com>
2009-08-13 18:56:02 +0200

    add header table option

--- Source/MooEditable/MooEditable.Table.js
+++ Source/MooEditable/MooEditable.Table.js
@@ -24,10 +24,10 @@ Author:
 */
 
 MooEditable.UI.TableDialog = function(editor){
-	var html = '<label class="dialog-label">Insert table</label>'
-		+ '<input type="text" class="table-r" value="" size="4" /> x '
-		+ '<input type="text" class="table-c" value="" size="4" />'
-		+ '<button class="dialog-button dialog-ok-button">OK</button>'
+	var html = 'columns <input type="text" class="table-c" value="" size="4" /> '
+		+ 'rows <input type="text" class="table-r" value="" size="4" /> '
+		+ '<input type="checkbox" class="table-h"/> header &nbsp; '
+		+ '<button class="dialog-button dialog-ok-button">Insert</button>'
 		+ '<button class="dialog-button dialog-cancel-button">Cancel</button>';
 	return new MooEditable.UI.Dialog(html, {
 		'class': 'mooeditable-prompt-dialog',
@@ -37,26 +37,27 @@ MooEditable.UI.TableDialog = function(editor){
 				input.focus();
 				input.select();
 			}).delay(10);
-			//console.log(editor.selection.getNode());
 		},
 		onClick: function(e){
-			e.preventDefault();
-			if (e.target.tagName.toLowerCase() != 'button') return;
+			if (e.target.tagName.toLowerCase() == 'button') e.preventDefault();
 			var button = document.id(e.target);
 			if (button.hasClass('dialog-cancel-button')) this.close();
 			else if (button.hasClass('dialog-ok-button')){
 				this.close();
-				var row = this.el.getElement('.table-r').value.toInt();
 				var col = this.el.getElement('.table-c').value.toInt();
+				var row = this.el.getElement('.table-r').value.toInt()+1;
+				var header = this.el.getElement('.table-h').checked;
 				if(!(row>0 && col>0)) return;
 				var div,table,tbody,ro = [];
 				div = new Element('tdiv');
 				table = new Element('table').set('border','0').set('width','100%').inject(div);
 				tbody = new Element('tbody').inject(table);
-				for(var r=0;r<row;r++){
+				var r = 1;
+				if(header) r = 0;
+				for(r;r<row;r++){
 					ro[r] = new Element('tr').inject(tbody,'bottom');
 					for(var c=0;c<col;c++){
-						new Element((r==0)?'th':'td').set('html',(r==0)?('header '+(c+1)):' ').inject(ro[r],'bottom');
+						new Element( r==0 ? 'th' : 'td' ).set('html', r==0 ? ('header '+(c+1)) : '&nbsp;' ).inject(ro[r],'bottom');
 					}
 				}
 				editor.selection.insertContent(div.get('html'));
