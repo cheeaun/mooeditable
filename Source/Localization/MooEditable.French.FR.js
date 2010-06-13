02654391776d7139cd7306e0dc68676872674ecb
Lim Chee Aun <cheeaun@gmail.com>
2010-06-13 18:31:08 +0800

    French locale by Roparz!

---
+++ Source/Localization/MooEditable.French.FR.js
@@ -0,0 +1,115 @@
+/*
+---
+ 
+script: MooEditable.French.FR.js
+ 
+description: MooEditable messages for FR French.
+ 
+license: MIT-style license
+ 
+authors:
+- Roparz
+ 
+requires:
+- MooEditable
+- more:1.2.4.2/Lang
+
+provides: [MooEditable.French.FR]
+
+...
+*/
+
+MooTools.lang.set('fr-FR', 'MooEditable', {
+	
+	// MooEditable
+	ok: 'OK',
+	cancel: 'Annuler',
+	bold: 'Gras',
+	italic: 'Italique',
+	underline: 'Souligné',
+	strikethrough: 'Barré',
+	unorderedList: 'Liste à puce',
+	orderedList: 'Liste ordonnée',
+	indent: 'Augmenter le retrait',
+	outdent: 'Diminuer le retrait',
+	undo: 'Annuler',
+	redo: 'Restaurer',
+	removeHyperlink: 'Supprimer le lien',
+	addHyperlink: 'Ajouter un lien',
+	selectTextHyperlink: 'Veuillez sélectionner le texte à lier.',
+	enterURL: 'Entrez une URL',
+	enterImageURL: 'Entrez l\'URL de l\'image',
+	addImage: 'Ajouter une image',
+	toggleView: 'Changer de vue',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: 'Ajouter un caratère spécial',
+	insertCharacter: 'Ajouter',
+	
+	// MooEditable.Extras
+	blockFormatting: 'Formatage',
+	paragraph: 'Paragraphe',
+	heading1: 'Titre 1',
+	heading2: 'Titre 2',
+	heading3: 'Titre 3',
+	alignLeft: 'Aligner à gauche',
+	alignRight: 'Aligner à droite',
+	alignCenter: 'Centrer',
+	alignJustify: 'Justifier',
+	removeFormatting: 'Supprimer le formatage',
+	insertHorizontalRule: 'Insérer une ligne horizontale',
+	
+	// MooEditable.Flash
+	embed: 'Entrer le code embed',
+	flashEmbed: 'Ajouter un objet flash',
+	
+	// MooEditable.Forecolor
+	changeColor: 'Changer la couleur',
+	
+	// MooEditable.Image
+	imageAlt: 'alt',
+	imageClass: 'classe',
+	imageAlign: 'alignement',
+	imageAlignNone: 'aucun',
+	imageAlignLeft: 'gauche',
+	imageAlignCenter: 'centre',
+	imageAlignRight: 'droite',
+	addEditImage: 'Ajouter/Editer une image',
+	
+	// MooEditable.Pagebreak
+	pageBreak: 'Saut de page',
+	
+	// MooEditable.Smiley
+	insertSmiley: 'Ajouter un Smiley',
+	
+	// MooEditable.Table
+	tableColumns: 'colonnes',
+	tableRows: 'lignes',
+	tableWidth: 'largeur',
+	tableClass: 'classe',
+	tableType: 'type',
+	tableHeader: 'entête',
+	tableCell: 'cellule',
+	tableAlign: 'alignement',
+	tableAlignNone: 'aucun',
+	tableAlignCenter: 'centre',
+	tableAlignRight: 'droite',
+	tableValign: 'alignement vertical',
+	tableValignNone: 'aucun',
+	tableValignTop: 'haut',
+	tableValignMiddle: 'milieu',
+	tableValignBottom: 'bas',
+	addTable: 'Ajouter un tableau',
+	editTable: 'Editer le tableau',
+	addTableRow: 'Ajouter une ligne',
+	editTableRow: 'Editer la ligne',
+	mergeTableRow: 'Scinder la ligne',
+	splitTableRow: 'Séparer la ligne',
+	deleteTableRow: 'Supprimer la ligne',
+	addTableCol: 'Ajouter une colonne',
+	editTableCol: 'Editer la colonne',
+	mergeTableCell: 'Scinder la colonne',
+	splitTableCell: 'Séprarer la colonne',
+	deleteTableCol: 'Supprimer la colonne'
+	
+});
