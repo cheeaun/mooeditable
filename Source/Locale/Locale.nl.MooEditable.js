e4315ff73234d893f65a72a02e45592a21f58c48
Erik Dubbelboer <erik@dubbelboer.com>
2011-03-07 23:57:59 +0800

    added dutch translation

---
+++ Source/Locale/Locale.nl.MooEditable.js
@@ -0,0 +1,124 @@
+/*
+---
+
+name: Locale.nl.MooEditable
+
+description: MooEditable messages in Dutch.
+
+license: MIT-style license
+
+authors:
+- Erik Dubbelboer
+
+requires:
+- MooEditable
+- more:1.3/Locale
+
+provides: [Locale.nl.MooEditable]
+
+...
+*/
+
+Locale.define('nl', 'MooEditable', {
+	
+	// MooEditable
+	ok: 'OK',
+	cancel: 'Annuleren',
+	bold: 'Vet',
+	italic: 'Cursief',
+	underline: 'Onderstrepen',
+	strikethrough: 'Doorhalen',
+	unorderedList: 'Opsommingstekens',
+	orderedList: 'Nummering',
+	indent: 'Inspringen',
+	outdent: 'Uitspringen',
+	undo: 'Ongedaan maken',
+	redo: 'Opnieuw',
+	removeHyperlink: 'Hyperlink verwijderen',
+	addHyperlink: 'Hyperlink toevoegen',
+	selectTextHyperlink: 'Selecteer eerst de tekst waar u de hyperlink aan toe wilt voegen',
+	enterURL: 'Voer de hyperlink in',
+	enterImageURL: 'Voer de hyperlink van de illustratie in',
+	addImage: 'Illustratie toevoegen',
+	toggleView: 'Wijzig modus',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: 'Voer speciaal symbool in',
+	insertCharacter: 'Voer symbool in',
+	
+	// MooEditable.Extras
+	blockFormatting: 'Alinea opmaak',
+	paragraph: 'Paragraaf',
+	heading1: 'Kop 1',
+	heading2: 'Kop 2',
+	heading3: 'Kop 3',
+	alignLeft: 'Links inspringen',
+	alignRight: 'Rechts inspringen',
+	alignCenter: 'Centreren',
+	alignJustify: 'Uitvullen',
+	removeFormatting: 'Verwijder opmaak',
+	insertHorizontalRule: 'Voer horizontale streep in',
+	
+	// MooEditable.Flash
+	embed: 'Voer embed code in',
+	flashEmbed: 'Flash Embed',
+	
+	// MooEditable.Forecolor
+	changeColor: 'Wijzig kleur',
+	
+	// MooEditable.Image
+	imageAlt: 'alt',
+	imageClass: 'class',
+	imageAlign: 'align',
+	imageAlignNone: 'none',
+	imageAlignLeft: 'left',
+	imageAlignCenter: 'center',
+	imageAlignRight: 'right',
+	addEditImage: 'Wijzig illustratie',
+	
+	// MooEditable.Pagebreak
+	pageBreak: 'Pagina einde',
+	
+	// MooEditable.Smiley
+	insertSmiley: 'Smiley toevoegen',
+	
+	// MooEditable.Table
+	tableColumns: 'kolommen',
+	tableRows: 'rijen',
+	tableWidth: 'breedte',
+	tableClass: 'class',
+	tableType: 'type',
+	tableHeader: 'Hoofd',
+	tableCell: 'Cel',
+	tableAlign: 'uitlijnen',
+	tableAlignNone: 'geen',
+	tableAlignCenter: 'midden',
+	tableAlignRight: 'rechts',
+	tableValign: 'verticaal uitlijnen',
+	tableValignNone: 'geen',
+	tableValignTop: 'boven',
+	tableValignMiddle: 'midden',
+	tableValignBottom: 'beneden',
+	addTable: 'Tabel toevoegen',
+	editTable: 'Tabel aanpassen',
+	addTableRow: 'Rij toevoegen',
+	editTableRow: 'Rij aanpassen',
+	mergeTableRow: 'Rijen samenvoegen',
+	splitTableRow: 'Rijen splitsen',
+	deleteTableRow: 'Rijen verwijderen',
+	addTableCol: 'Kolom toevoegen',
+	editTableCol: 'Kolom aanpassen',
+	mergeTableCell: 'Cellen samenvoegen',
+	splitTableCell: 'Cel splitsen',
+	deleteTableCol: 'Kolom verwijderen',
+    
+    // MooEditable.UI.ExtendedLinksDialog
+    protocol: 'protocol',
+    link: 'hyperlink',
+    email: 'e-Mail',
+    urlWithoutHttp: 'Hyperlink (zonder http://)',
+    window: 'Venster',
+    sameWindow: 'Zelfde venster',
+    newWindow: 'Nieuw venster'
+    
+});
