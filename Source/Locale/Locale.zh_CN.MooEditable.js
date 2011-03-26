2341b1a127ef039158d6628674ff6fd44898ef77
fancyoung <siyang1982@msn.com>
2011-03-26 17:51:07 +0800

    add Chinese locale

---
+++ Source/Locale/Locale.zh_CN.MooEditable.js
@@ -0,0 +1,103 @@
+Locale.define('zh-CN', 'MooEditable', {
+	
+	// MooEditable
+	ok: '确定',
+	cancel: '取消',
+	bold: '粗体',
+	italic: '斜体',
+	underline: '下划线',
+	strikethrough: '删除线',
+	unorderedList: '项目符号',
+	orderedList: '编号',
+	indent: '增加缩进量',
+	outdent: '减少缩进量',
+	undo: '撤销',
+	redo: '重做',
+	removeHyperlink: '删除链接',
+	addHyperlink: '添加链接',
+	selectTextHyperlink: '请选择要添加链接的文字',
+	enterURL: '输入链接',
+	enterImageURL: '输入图片链接',
+	addImage: '添加图片',
+	toggleView: '切换视图',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: '插入自定义字符',
+	insertCharacter: '插入字符',
+	
+	// MooEditable.Extras
+	blockFormatting: '格式',
+	paragraph: '正文',
+	heading1: '标题1',
+	heading2: '标题2',
+	heading3: '标题3',
+	alignLeft: '左对齐',
+	alignRight: '右对齐',
+	alignCenter: '居中',
+	alignJustify: '两端对齐',
+	removeFormatting: '删除格式',
+	insertHorizontalRule: '插入分割线', 
+	
+	// MooEditable.Flash
+	embed: '输入嵌入代码',
+	flashEmbed: '嵌入Flash',
+	
+	// MooEditable.Forecolor
+	changeColor: '更改颜色',
+	
+	// MooEditable.Image
+	imageAlt: 'alt',
+	imageClass: 'class',
+	imageAlign: 'align',
+	imageAlignNone: 'none',
+	imageAlignLeft: 'left',
+	imageAlignCenter: 'center',
+	imageAlignRight: 'right',
+	addEditImage: '添加/编辑图片',
+	
+	// MooEditable.Pagebreak
+	pageBreak: '分页符',
+	
+	// MooEditable.Smiley
+	insertSmiley: '插入表情',
+	
+	// MooEditable.Table
+	tableColumns: 'columns',
+	tableRows: 'rows',
+	tableWidth: 'width',
+	tableClass: 'class',
+	tableType: 'type',
+	tableHeader: 'Header',
+	tableCell: 'Cell',
+	tableAlign: 'align',
+	tableAlignNone: 'none',
+	tableAlignCenter: 'center',
+	tableAlignRight: 'right',
+	tableValign: 'vertical align',
+	tableValignNone: 'none',
+	tableValignTop: 'top',
+	tableValignMiddle: 'middle',
+	tableValignBottom: 'bottom',
+	addTable: '新建表格',
+	editTable: '编辑表格',
+	addTableRow: '添加行',
+	editTableRow: '编辑行',
+	mergeTableRow: '合并行',
+	splitTableRow: '拆分行',
+	deleteTableRow: '增加行',
+	addTableCol: '增加列',
+	editTableCol: '编辑列',
+	mergeTableCell: '合并单元格',
+	splitTableCell: '拆分单元格',
+	deleteTableCol: '删除列',
+    
+    // MooEditable.UI.ExtendedLinksDialog
+    protocol: 'protocol',
+    link: 'link',
+    email: 'e-Mail',
+    urlWithoutHttp: 'URL (without http://)',
+    window: '窗口',
+    sameWindow: '当前窗口',
+    newWindow: '新窗口'
+    
+});
