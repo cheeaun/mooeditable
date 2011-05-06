40fcb15b387828848ffcb97559c63640853548f5
Rodrigo Panachi <rodrigopanachi@gmail.com>
2011-05-06 05:25:44 -0300

    Language for Portuguese (Brazilian)

---
+++ Source/Locale/Locale.pt-BR.MooEditable.js
@@ -0,0 +1,124 @@
+/*
+---
+
+name: Locale.pt-BR.MooEditable
+
+description: MooEditable messages for PT Brazilian.
+
+license: MIT-style license
+
+authors:
+- Rodrigo Panachi
+
+requires:
+- MooEditable
+- more:1.3/Locale
+
+provides: [Locale.pt-BR.MooEditable]
+
+...
+*/
+
+Locale.define('pt-BR', 'MooEditable', {
+	
+	// MooEditable
+	ok: 'OK',
+	cancel: 'Cancelar',
+	bold: 'Negrito',
+	italic: 'Itálico',
+	underline: 'Sublinhado',
+	strikethrough: 'Tachado',
+	unorderedList: 'Lista sem números',
+	orderedList: 'Lista numerada',
+	indent: 'Aumentar recuo',
+	outdent: 'Diminuir recuo',
+	undo: 'Desfazer',
+	redo: 'Refazer',
+	removeHyperlink: 'Remover link',
+	addHyperlink: 'Inserir link',
+	selectTextHyperlink: 'Por favor, selecione o texto que deseja inserir o link.',
+	enterURL: 'Forneça a URL',
+	enterImageURL: 'Forneça a URL da imagem',
+	addImage: 'Adicionar imagem',
+	toggleView: 'Alterar visualização',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: 'Inserir caractere personalizado',
+	insertCharacter: 'Inserir caractere',
+	
+	// MooEditable.Extras
+	blockFormatting: 'Formatar bloco',
+	paragraph: 'Parágrafo',
+	heading1: 'Título 1',
+	heading2: 'Título 2',
+	heading3: 'Titulo 3',
+	alignLeft: 'Alinhar esquerda',
+	alignRight: 'Alinhar direita',
+	alignCenter: 'Centralizar',
+	alignJustify: 'Justificado',
+	removeFormatting: 'Remover formatação',
+	insertHorizontalRule: 'Inserir linha horizontal',
+	
+	// MooEditable.Flash
+	embed: 'Forneça código embutido',
+	flashEmbed: 'Flash embutido',
+	
+	// MooEditable.Forecolor
+	changeColor: 'Alterar cor',
+	
+	// MooEditable.Image
+	imageAlt: 'Alt',
+	imageClass: 'Classe',
+	imageAlign: 'Alinhamento',
+	imageAlignNone: 'nenhum',
+	imageAlignLeft: 'esquerda',
+	imageAlignCenter: 'centralizado',
+	imageAlignRight: 'direita',
+	addEditImage: 'Inserir/editar imagem',
+	
+	// MooEditable.Pagebreak
+	pageBreak: 'Quebra de página',
+	
+	// MooEditable.Smiley
+	insertSmiley: 'Inserir Smiley',
+	
+	// MooEditable.Table
+	tableColumns: 'Colunas',
+	tableRows: 'Linhas',
+	tableWidth: 'Largura',
+	tableClass: 'Classe',
+	tableType: 'Tipo',
+	tableHeader: 'Cabeçalho',
+	tableCell: 'Célula',
+	tableAlign: 'Alinhamento',
+	tableAlignNone: 'nenhum',
+	tableAlignCenter: 'centralizado',
+	tableAlignRight: 'direita',
+	tableValign: 'Alinhamento vertical',
+	tableValignNone: 'nenhum',
+	tableValignTop: 'topo',
+	tableValignMiddle: 'meio',
+	tableValignBottom: 'inferior',
+	addTable: 'Inserir tabela',
+	editTable: 'Editar tabela',
+	addTableRow: 'Inserir linha',
+	editTableRow: 'Editar linha',
+	mergeTableRow: 'Mesclar linha',
+	splitTableRow: 'Dividir linha',
+	deleteTableRow: 'Excluir linha',
+	addTableCol: 'Inserir coluna',
+	editTableCol: 'Editar coluna',
+	mergeTableCell: 'Mesclar célula',
+	splitTableCell: 'Dividir célula',
+	deleteTableCol: 'Excluir coluna',
+    
+    // MooEditable.UI.ExtendedLinksDialog
+    protocol: 'Protocolo',
+    link: 'Link',
+    email: 'E-Mail',
+    urlWithoutHttp: 'URL (sem http://)',
+    window: 'Janela',
+    sameWindow: 'Mesma janela',
+    newWindow: 'Nova janela'
+    
+});
