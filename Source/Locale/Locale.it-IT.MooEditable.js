57b0df50d58ac442c5bd0ad8688eb5c80fd141a4
Daniele Corti <daniele@imac-di-daniele.gestweb>
2012-02-02 09:27:52 +0100

    new file:   Source/Locale/Locale.it-IT.MooEditable.js
    This add Localization for MooEditable in Italian.

---
+++ Source/Locale/Locale.it-IT.MooEditable.js
@@ -0,0 +1,115 @@
+/*
+---
+
+name: Locale.it-IT.MooEditable
+
+description: MooEditable messages for French.
+
+license: MIT-style license
+
+authors:
+- danielecorti81
+
+requires:
+- MooEditable
+- more:1.3/Locale
+
+provides: [Locale.it-IT.MooEditable]
+
+...
+*/
+
+Locale.define('it-IT', 'MooEditable', {
+	
+	// MooEditable
+	ok: 'OK',
+	cancel: 'Annullare',
+	bold: 'Grassetto',
+	italic: 'Corsivp',
+	underline: 'Sottolineato',
+	strikethrough: 'Barrato',
+	unorderedList: 'Lista non ordinata',
+	orderedList: 'Lista ordinata',
+	indent: 'Aumentare il rientro',
+	outdent: 'Diminuire il rientro',
+	undo: 'Annulla',
+	redo: 'Ripristina',
+	removeHyperlink: 'Rimuovere il link',
+	addHyperlink: 'Aggiungere un link',
+	selectTextHyperlink: 'Prego selezionare il testo da includere nel link.',
+	enterURL: 'Inserire un URL',
+	enterImageURL: 'Inserire l\'URL dell\'immagine',
+	addImage: 'Aggiungere un\'immagine',
+	toggleView: 'Cambia la Vista',
+	
+	// MooEditable.Charmap
+	insertCustomCharacter: 'Aggiungi un carattere speciale',
+	insertCharacter: 'Aggiungi',
+	
+	// MooEditable.Extras
+	blockFormatting: 'Blocca la Formattazione',
+	paragraph: 'Paragrafo',
+	heading1: 'Titolo 1',
+	heading2: 'Titolo 2',
+	heading3: 'Titolo 3',
+	alignLeft: 'Allineamento a sinistra',
+	alignRight: 'Allineamento a destra',
+	alignCenter: 'Allineamento centrato',
+	alignJustify: 'Allineamento Giustificato',
+	removeFormatting: 'Rimuovere la Formattazione',
+	insertHorizontalRule: 'Inserimento di una Linea Orizzontale',
+	
+	// MooEditable.Flash
+	embed: 'Inserire il codice da incorporare',
+	flashEmbed: 'Aggiungere un oggetto flash',
+	
+	// MooEditable.Forecolor
+	changeColor: 'Cambiare il colore',
+	
+	// MooEditable.Image
+	imageAlt: 'alt',
+	imageClass: 'classe',
+	imageAlign: 'allineamento',
+	imageAlignNone: 'nessuno',
+	imageAlignLeft: 'sinistra',
+	imageAlignCenter: 'centrato',
+	imageAlignRight: 'destra',
+	addEditImage: 'Aggiungere/Modificare un\'immagine',
+	
+	// MooEditable.Pagebreak
+	pageBreak: 'Interruzione di Pagina',
+	
+	// MooEditable.Smiley
+	insertSmiley: 'Ajouter un Smiley',
+	
+	// MooEditable.Table
+	tableColumns: 'colonne',
+	tableRows: 'righe',
+	tableWidth: 'larghezza',
+	tableClass: 'classe',
+	tableType: 'tipo',
+	tableHeader: 'intestazione',
+	tableCell: 'cella',
+	tableAlign: 'allineamento',
+	tableAlignNone: 'nessuno',
+	tableAlignCenter: 'centrato',
+	tableAlignRight: 'destra',
+	tableValign: 'allineamento verticale',
+	tableValignNone: 'nessuno',
+	tableValignTop: 'in testa',
+	tableValignMiddle: 'al centro',
+	tableValignBottom: 'in fondo',
+	addTable: 'Aggiungere una tabella',
+	editTable: 'Modificare una tabella',
+	addTableRow: 'Aggiungere una riga',
+	editTableRow: 'Modificare una riga',
+	mergeTableRow: 'Unire la riga',
+	splitTableRow: 'Dividere la riga',
+	deleteTableRow: 'Cancellare la riga',
+	addTableCol: 'Aggiungere una colonna',
+	editTableCol: 'Modificare la colonna',
+	mergeTableCell: 'Unire la colonna',
+	splitTableCell: 'Dividere la colonna',
+	deleteTableCol: 'Cancellare la colonna'
+	
+});
